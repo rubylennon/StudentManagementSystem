61369d9ac0202707c4a528304b9766c1e696bfd6
Ruby Lennon <x19128355@student.ncirl.ie>
2023-03-30 16:25:24 +0100

    fix database schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2023_03_27_194547) do
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "comments", force: :cascade do |t|
     t.text "content", null: false
     t.integer "course_module_id", null: false
